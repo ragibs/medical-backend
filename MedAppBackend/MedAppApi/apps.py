59d15580a45b14886fa5b0d75b1f800ed42ce1ce
Suchet Mangat <136658794+SuchetMangat@users.noreply.github.com>
2024-10-19 20:05:32 -0600

    testing git ignore

---
+++ MedAppBackend/MedAppApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedappapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MedAppApi'
---
+++ MedAppBackend/dbcreation.py
@@ -0,0 +1,31 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
+
+import mysql.connector
+# from dotenv import load_dotenv
+from dotenv import dotenv_values
+import os
+
+# Loading Enviornment Variables
+env_path = os.path.join('..', 'Virtual', '.env')
+secrets = dotenv_values(env_path)
+# load_dotenv(env_path)
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    # user = os.getenv('SQL_USERNAME'),
+    # passwd = os.getenv('SQL_PASSWORD')
+    user = secrets['SQL_USERNAME'],
+    passwd = secrets['SQL_PASSWORD']
+)
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE medicalapp")
+
+print("Database Created")
