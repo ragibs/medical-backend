53214624ecf89d1939ade4f9335ec9d94ffda3fb
Suchet Mangat <136658794+SuchetMangat@users.noreply.github.com>
2024-11-03 22:33:18 -0700

    Added serializers

--- MedAppBackend/MedAppApi/serializers.py
+++ MedAppBackend/MedAppApi/serializers.py
@@ -4,17 +4,124 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from .models import *
 
-# serializers.HyperlinkedModelSerializer
-class DoctorSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
-        model = Doctor
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            first_name= validated_data['first_name']
+            last_name= validated_data['last_name']
+            email= validated_data['email']
+            username= validated_data['username']
+            password= validated_data['password']
+        )
+    
+
+class DoctorRegistrationSerializer(serializers.ModelSerializer):
+    user = UserRegistrationSerializer()
+
+    class Meta:
+        model = User
         fields = '__all__'
 
-class PatientSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserRegistrationSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+        doctor = Doctor.objects.create(user=user, **validated_data)
+        return doctor
+
+
+class PatientRegistrationSerializer(serializers.ModelSerializer):
+    user = UserRegistrationSerializer()
+
     class Meta:
-        model = Patient
+        model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserRegistrationSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+        patient = Patient.objects.create(user=user, **validated_data)
+        return patient
+
+
+class AdminStaffRegistrationSerializer(serializers.ModelSerializer):
+    user = UserRegistrationSerializer()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserRegistrationSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+        admin_staff = AdminStaff.objects.create(user=user, **validated_data)
+        return admin_staff
+    
+class AdminStaffRegistrationSerializer(serializers.ModelSerializer):
+    user = UserRegistrationSerializer()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserRegistrationSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+        admin_staff = AdminStaff.objects.create(user=user, **validated_data)
+        return admin_staff
+
+
+class PatientDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientDetails
+        fields = '__all__'
+
+
+class AvailabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = '__all__'
+
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = '__all__'
+
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = '__all__'
+
+
+class TestimonialsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testimonials
+        fields = '__all__'
+
+
+class RatingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testimonials
+        fields = '__all__'
+
+
+
+
+# For Reference only serves no purpose in backend implementation
 class TestUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestUser
