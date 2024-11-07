fb1e3f59bf1651a10407f0d9abc6f2de311a4578
ragib sina <ragib.sina@gmail.com>
2024-11-07 16:43:07 -0700

    add login, register, roles, appointments

--- MedAppBackend/MedAppApi/serializers.py
+++ MedAppBackend/MedAppApi/serializers.py
@@ -6,96 +6,47 @@ from rest_framework import serializers
 from .models import *
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'username', 'password']
-
-    def create(self, validated_data):
-        return User.objects.create_user(
-            first_name= validated_data['first_name'],
-            last_name= validated_data['last_name'],
-            email= validated_data['email'],
-            username= validated_data['username'],
-            password= validated_data['password'],
-        )
-    
-
-class DoctorRegistrationSerializer(serializers.ModelSerializer):
-    user = UserRegistrationSerializer()
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user_serializer = UserRegistrationSerializer(data=user_data)
-        user_serializer.is_valid(raise_exception=True)
-        user = user_serializer.save()
-        doctor = Doctor.objects.create(user=user, **validated_data)
-        return doctor
-
-
-class PatientRegistrationSerializer(serializers.ModelSerializer):
-    user = UserRegistrationSerializer()
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user_serializer = UserRegistrationSerializer(data=user_data)
-        user_serializer.is_valid(raise_exception=True)
-        user = user_serializer.save()
-        patient = Patient.objects.create(user=user, **validated_data)
-        return patient
-    
-
 class AuthPatientRegistrationSerializer(RegisterSerializer):
-    # User Information
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
-    # Patient Information
     phone = serializers.CharField(max_length=15)
     address = serializers.CharField(max_length=100)
     city = serializers.CharField(max_length=50)
     state = serializers.CharField(max_length=50)
     zipcode = serializers.CharField(max_length=15)
+    date_of_birth = serializers.DateField(required=True)
+
+
+    def validate_username(self, username):
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError("A user with this username already exists.")
+        return username
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return email
 
     def custom_signup(self, request, user):
-        # Alternative syntax for assigning default vals
-        # user.first_name = self.validated_data.get('first_name', '')
+        # Set user's first and last name
         user.first_name = self.validated_data['first_name']
         user.last_name = self.validated_data['last_name']
         user.save()
 
-        patientobject_data = {
-            **self.validated_data,
-            'user': user,
-            'appointments_booked': 0
-        }
+        # Create the profile with role
+        UserProfile.objects.create(user=user, role='PATIENT')
 
-        patientobject_data.pop('username')
-        patientobject_data.pop('email')
-        patientobject_data.pop('password1')
-        patientobject_data.pop('password2')
-        patientobject_data.pop('first_name')
-        patientobject_data.pop('last_name')
-        Patient.objects.create(**patientobject_data)
+        # Create the patient model instance
+        Patient.objects.create(
+            user=user,
+            phone=self.validated_data['phone'],
+            address=self.validated_data['address'],
+            city=self.validated_data['city'],
+            state=self.validated_data['state'],
+            zipcode=self.validated_data['zipcode'],
+            date_of_birth=self.validated_data['date_of_birth'],
+        )
 
-        # Alternative Syntax for creating patient object:
-        # Patient.objects.create(
-        #     user=user,
-        #     phone=self.validated_data['phone'],
-        #     address=self.validated_data['address'],
-        #     city=self.validated_data['city'],
-        #     state=self.validated_data['state'],
-        #     zipcode=self.validated_data['zipcode'],
-        #     appointments_booked=0  # Default value for new registration
-        # )
 
 
 class AuthDoctorRegistrationSerializer(RegisterSerializer):
@@ -110,13 +61,19 @@ class AuthDoctorRegistrationSerializer(RegisterSerializer):
     state = serializers.CharField(max_length=50)
     zipcode = serializers.CharField(max_length=15)
     bio = serializers.CharField()
+    short_bio = serializers.CharField(max_length=150)
     years_experience = serializers.IntegerField()
 
     def custom_signup(self, request, user):
+        # Set the user's first and last name
         user.first_name = self.validated_data['first_name']
         user.last_name = self.validated_data['last_name']
         user.save()
 
+        # Create a UserProfile for role management
+        UserProfile.objects.create(user=user, role='DOCTOR')
+
+        # Create a Doctor instance
         Doctor.objects.create(
             user=user,
             phone=self.validated_data['phone'],
@@ -126,9 +83,11 @@ class AuthDoctorRegistrationSerializer(RegisterSerializer):
             state=self.validated_data['state'],
             zipcode=self.validated_data['zipcode'],
             bio=self.validated_data['bio'],
-            years_experience=self.validated_data['years_experience'],  
+            short_bio=self.validated_data['short_bio'],
+            years_experience=self.validated_data['years_experience'],
         )
 
+# Admin Staff Registration Serializer
 class AuthAdminStaffRegistrationSerializer(RegisterSerializer):
     # User Information
     first_name = serializers.CharField(required=True)
@@ -137,82 +96,44 @@ class AuthAdminStaffRegistrationSerializer(RegisterSerializer):
     title = serializers.CharField(max_length=50)
 
     def custom_signup(self, request, user):
+        # Set the user's first and last name
         user.first_name = self.validated_data['first_name']
         user.last_name = self.validated_data['last_name']
         user.save()
 
+        # Create a UserProfile for role management
+        UserProfile.objects.create(user=user, role='ADMIN')
+
+        # Create an AdminStaff instance
         AdminStaff.objects.create(
             user=user,
-            title=self.validated_data['title'], 
+            title=self.validated_data['title'],
         )
-
-
-class AdminStaffRegistrationSerializer(serializers.ModelSerializer):
-    user = UserRegistrationSerializer()
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user_serializer = UserRegistrationSerializer(data=user_data)
-        user_serializer.is_valid(raise_exception=True)
-        user = user_serializer.save()
-        admin_staff = AdminStaff.objects.create(user=user, **validated_data)
-        return admin_staff
-    
-class AdminStaffRegistrationSerializer(serializers.ModelSerializer):
-    user = UserRegistrationSerializer()
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user_serializer = UserRegistrationSerializer(data=user_data)
-        user_serializer.is_valid(raise_exception=True)
-        user = user_serializer.save()
-        admin_staff = AdminStaff.objects.create(user=user, **validated_data)
-        return admin_staff
-
-
-class PatientDetailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PatientDetails
-        fields = '__all__'
-
-
-class AvailabilitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Availability
-        fields = '__all__'
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = '__all__'
-        
+        fields = [
+            'id',
+            'patient',
+            'doctor',
+            'date',
+            'time',
+            'symptoms',
+            'ai_summarized_symptoms',
+            'notes',
+        ]
+        read_only_fields = ['ai_summarized_symptoms']  # Set this field as read-only
 
+    def validate(self, data):
+        # Custom validation for time slots, preventing double-booking for the same doctor
+        doctor = data.get('doctor')
+        date = data.get('date')
+        time = data.get('time')
 
-class TestimonialsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Testimonials
-        fields = '__all__'
+        # Check if there's already an appointment for the doctor at the same date and time
+        if Appointment.objects.filter(doctor=doctor, date=date, time=time).exists():
+            raise serializers.ValidationError("This time slot is already booked for the selected doctor.")
 
-
-class RatingsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Testimonials
-        fields = '__all__'
-
-
-
-
-# For Reference only serves no purpose in backend implementation
-class TestUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TestUser
-        fields = '__all__'
+        return data
--- MedAppBackend/MedAppApi/urls.py
+++ MedAppBackend/MedAppApi/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path, include
-from dj_rest_auth.views import LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 from .views import *
 
 urlpatterns = [
-    # Placeholder
-    path('', views.home, name='home'),
-    path('testusers/', get_testusers, name='get_testusers'),
-    path('testusers/create/', create_testuser, name='create_testuser'),
-    path('testusers/<int:pk>', update_testuser, name='update_testuser'),
-    path('login', LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('register/patient/method', register_patient_method, name='register_patient_method'),
-    path('register/patient/class', RegisterPatientClass.as_view(), name='register_patient_class'),
-    path('register/doctor', register_doctor, name='register_doctor'),
-    path('register/adminstaff', register_adminstaff, name='register_adminstaff'),
-    path('make/appointment', make_appointment, name='make_appointment'),
+    
+    # Registration endpoints
+    path('register/patient/', register_patient, name='register_patient'),
+    path('register/doctor/', register_doctor, name='register_doctor'),
+    path('register/adminstaff/', register_adminstaff, name='register_adminstaff'),
+    
+    # Appointment endpoint
+    path('make/appointment/', make_appointment, name='make_appointment'),
+
 ]
