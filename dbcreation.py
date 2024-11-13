89fb0c88cb1cd323c37832e611081c9d344a4d5b
Remi Oyediji <remioyediji@Remis-MacBook-Pro.local>
2024-11-13 09:11:03 -0700

    Modified method for obtaining environment variables

---
+++ dbcreation.py
@@ -0,0 +1,41 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
+
+import mysql.connector
+from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
+
+
+# Loading Enviornment Variables
+# env_path = os.path.join('..', 'Virtual', '.env')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / '.env'
+
+# secrets = dotenv_values(env_path)
+
+load_dotenv(env_path)
+
+# dataBase = mysql.connector.connect(
+#     host = 'localhost',
+#     user = secrets['SQL_USERNAME'],
+#     passwd = secrets['SQL_PASSWORD']
+# )
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = os.getenv('MYSQL_USER'),
+    passwd = os.getenv('MYSQL_ROOT_PASSWORD')
+)
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE medicalapp")
+
+print("Database Created")
