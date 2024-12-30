1624a1672098f8a856a6e054e165d15a97bcc6ad
ragib sina <ragib.sina@gmail.com>
2024-12-30 11:08:30 -0700

    remoed port dockerfile

--- MedAppBackend/MedAppBackend/settings.py
+++ MedAppBackend/MedAppBackend/settings.py
@@ -201,7 +201,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://23.17.49.58', #ubuntu server ip
     'https://127.0.0.1', # Localhost for internal requests
     'http://localhost:3000',  # localhost frontend
-    'https://medical-frontend-kappa.vercel.app'  # vercel frontend
+    'https://medical-frontend-kappa.vercel.app',  # vercel frontend
+    'https://medical-server.houseofmubina.com'
 ]
 CORS_ALLOW_HEADERS = [
     'authorization',
